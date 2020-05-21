22f5cf9796bf29c7e4f2329cb6417ce8c932d908
ZachZheng <robo_tsky@126.com>
2020-05-21 11:25:46 +0800

    添加注释

--- guide/tutorial/Step12/CTestConfig.cmake
+++ guide/tutorial/Step12/CTestConfig.cmake
@@ -1,6 +1,8 @@
+# 项目名称和时间
 set(CTEST_PROJECT_NAME "CMakeTutorial")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
+# dashboard 的地址
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=CMakeTutorial")
